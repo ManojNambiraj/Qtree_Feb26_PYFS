5cf774c1bca0867a5a67951eb23f59b72405b597
manojnambiraj <postboxmano@gmail.com>
2026-02-25 20:45:44 +0530

    JS intro - operators

---
+++ Javascript/Class1/script.js
@@ -0,0 +1,124 @@
+// Output statement
+
+    // alert()
+    // console.log()
+    // DOM
+
+// Javascript (ES6) --> EcmaScript
+// Old --> ES5
+
+// Variables: (Rules)
+
+    // myAgeInSchools = 100
+
+    // my_age_in_schools = 100
+
+    // console.log(myAgeInSchools)
+
+// Hoisting: (Variable Declaration)
+
+    // var --> ES5
+
+        // var a = 10
+
+        // var a = 20
+
+        // console.log(a);
+    
+    // let --> ES6
+
+        // let a
+
+        // a = 200
+        // a = 1000
+
+        // console.log(a);
+    
+    // const
+
+        // const a = 20
+
+        // console.log(a);
+    
+// Scoping:
+
+    // Global Scope
+
+        // {
+        //     var a = 100
+
+        //     console.log("inside: ", a);
+            
+        // }
+
+        //  console.log("outside: ", a);
+
+    // Local Scope
+
+        // {
+        //     let a = 100
+
+        //     console.log("inside: ", a);
+            
+        // }
+
+        // console.log("outside: ", a);
+
+// Datatypes:
+
+    // let a = 1000
+    // let a = 10.555
+    // let a = 'hello'
+    // let a = true // false
+    // let a
+    // let a = null
+
+    // console.log(a, typeof(a))
+
+// Operators:
+
+    // Arithmetic op
+        // (+, -, *, /, %, **, ++, --)
+
+        // let a = 5
+        // let b = 3
+        // let result
+
+        // result = a + b
+        // result = a - b
+        // result = a * b
+        // result = a / b
+        // result = a % b
+        // result = a ** b
+        // result = ++a
+        // result = --a
+
+        // console.log(result);
+
+    // Assignment op
+        // (=, +=, -=, *=, /=, %=, **=)
+
+        // let a = 5
+
+        // a += 3    // a = a + 3
+
+        // console.log(a);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Comparison op
+    // Logical op
+    // Ternary op
+    // Typeof op
