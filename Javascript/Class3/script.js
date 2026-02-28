151611293b00710a6edb567d55cdae9ad4fcbcb4
manojnambiraj <postboxmano@gmail.com>
2026-02-28 20:50:20 +0530

    Control stmts

---
+++ Javascript/Class3/script.js
@@ -0,0 +1,135 @@
+// Control Statements:
+    // Conditional stmts
+
+        // if
+
+            // let age = 23
+
+            // if(age > 18){
+            //     console.log("Eligible for voting");
+            // }
+
+        // if else
+
+            // let age = 17
+
+            // if(age > 18){
+            //     console.log("Eligible for voting");
+            // } else {
+            //     console.log("Not Eligible");
+            // }
+
+        // if else if
+
+            // let age = 18
+
+            // if(age > 18){
+            //     console.log("Eligible for voting");
+            // } else if(age == 18){
+            //     console.log("partially eligible");
+            // } else if(age < 18){
+            //     console.log("Not Eligible");
+            // } else {
+            //     console.log("Something went wrong");
+            // }
+
+        // switch case
+
+            // let day = 15
+
+            // switch(day){
+            //     case 1:
+            //         console.log("It's a Monday");
+            //         break;
+            //     case 2:
+            //         console.log("It's a Tuesday");
+            //         break;
+            //     case "Wed":
+            //         console.log("It's a Wednesday");
+            //         break;
+            //     case 4:
+            //         console.log("It's a Thursday");
+            //         break;
+            //     case 5:
+            //         console.log("It's a Friday");
+            //         break;
+            //     case 6:
+            //         console.log("It's a Saturday");
+            //         break;
+            //     default:
+            //         console.log("Happy Weekend");
+            // }
+
+    // Looping stmts
+
+        // for loop
+
+            // for(let i = 1; i <= 10; i++){
+            //     console.log(i);
+            // }
+
+            // for(let i = 10; i >= 1; i--){
+            //     console.log(i);
+            // }
+
+        // while loop
+
+            // let i = 1
+
+            // while(i <= 10){
+            //     console.log(i);
+            //     i++;
+            // }
+
+        // do...while loop
+
+            // let i = 11
+
+            // do{
+            //     console.log(i);
+            //     i++;
+            // }while(i <= 5)
+
+
+    // Jumping stmts
+
+        // break
+
+            // let i = 1
+
+            // while(i <= 5){
+            
+            //     if(i == 3){
+            //         break;
+            //     }
+
+            //     console.log(i);
+            //     i++;
+            // }
+ 
+        // continue
+
+            // let i = 1
+
+            // while(i <= 5){
+                
+            //     if(i == 3){
+            //         i++;
+            //         continue;
+            //     }
+
+            //     console.log(i);
+            //     i++;
+            
+            // }
+
+            // console.log("----------------------------");
+
+
+            // for(let i = 1; i <= 5; i++){
+            //     if(i == 3){
+            //         continue
+            //     }
+
+            //     console.log(i);
+            // }
