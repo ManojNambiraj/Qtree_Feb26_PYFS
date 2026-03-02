37f1e7de2ed3b8d003a9724161a3371da5fe098c
manojnambiraj <postboxmano@gmail.com>
2026-03-02 20:43:25 +0530

    JS Functions

---
+++ Javascript/Class4/script.js
@@ -0,0 +1,63 @@
+// Functions
+
+    // Built-in functions
+
+        // alert("hi")
+        // console.log("hi");
+
+    // User defined functions
+
+// 1. Normal Type function
+
+        // function demo(){                     // Definition
+        //     console.log("Hello world");
+        // }
+
+        // demo()                               // Function call
+
+    // Parameterized function
+
+        // function demo(a, b){
+        //     console.log(a, b);
+        // }
+
+        // demo(100, [1,2,3,4,5,6,6])
+        // demo(200, {name: "kavitha"})
+        // demo(7)
+        // demo(5)
+
+
+            // function Add(a, b){
+            //     return a + b
+            // }
+
+            // console.log(Add(2, 7));
+            // console.log(Add(100, 50));
+
+            // function Mul(x, y){
+            //     return x * y
+            // }
+
+            // var result = Mul(2, 5)
+
+            // console.log(result);
+
+// 2. Ananymous Functions (Un-named)
+
+    // let demo = function(a){
+    //     console.log(a);
+    // }
+
+    // demo("Hello world")
+
+// 3. Arrow functions: (ES6)
+
+    // let arr = () => "Hello, I'm arrow"
+
+    // console.log(arr());
+
+// 4. IIFE (Immediatly invoked function Expression)
+
+    // (() => {
+    //     console.log("I'm IIFE");
+    // })()
